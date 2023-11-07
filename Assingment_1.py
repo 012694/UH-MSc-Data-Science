78525653389bde6afaf9ad678d32d9120b1b2498
012694 <148860859+012694@users.noreply.github.com>
2023-11-07 17:47:05 +0000

    Add files via upload

--- Assingment_1.py
+++ Assingment_1.py
@@ -49,94 +49,121 @@ Plotting the data as three different graphs to extract meaningful information.
  
 # Plotting the annual gross wages per qualifications by ages as a line graph
 
-plt.figure()
-plt.plot(t_grad_annual_wage.index[0::], t_grad_annual_wage['Graduates'], 
+def grad_line_graph(df):
+    plt.figure()
+    plt.plot(t_grad_annual_wage.index[0::], t_grad_annual_wage['Graduates'], 
          label=('Graduate'))
-plt.plot(t_grad_annual_wage.index[0::], t_grad_annual_wage['Apprenticeship'],
+    plt.plot(t_grad_annual_wage.index[0::], t_grad_annual_wage['Apprenticeship'],
          label=('Apprenticeship'))
-plt.plot(t_grad_annual_wage.index[0::], t_grad_annual_wage['A level'], 
+    plt.plot(t_grad_annual_wage.index[0::], t_grad_annual_wage['A level'], 
          label=('A level'))
-plt.plot(t_grad_annual_wage.index[0::], t_grad_annual_wage['A* to C grade GCSE'],
+    plt.plot(t_grad_annual_wage.index[0::], t_grad_annual_wage['A* to C grade GCSE'],
          label=('GCSE'))
 
 
 # Setting the limits and ticks on the X axis for the graph 
-plt.xlim(21, 30)
-plt.ylim(10000, 40000)
-x_ticks = np.arange(21, 70, 5)  
-plt.xticks(x_ticks)
+    plt.xlim(21, 30)
+    plt.ylim(10000, 40000)
+    x_ticks = np.arange(21, 70, 5)  
+    plt.xticks(x_ticks)
 
 
 # Setting the legend and labels for the graph
-plt.legend() 
-plt.xlabel('Ages')
-plt.ylabel('Annual Wages')
-plt.title(' Average Salaries in UK(2017) by Ages and Highest Educ Qualification')
-plt.savefig('Assign1_Lineplot.png')
-plt.show()
+    plt.legend() 
+    plt.xlabel('Ages')
+    plt.ylabel('Annual Wages')
+    plt.title(' Average Salaries in UK(2017) by Ages and Highest Educ Qualification')
+    plt.savefig('Assign1_Lineplot.png')
+    plt.show()
+    return
 
-
+# Calling the function grad_line_graph 
+grad_line_graph(t_grad_annual_wage)
 
 
 """
 The second graph is a Box plot showing the median, modal and quartile salaries,
- for each qualification. The box plot also shows outlier salaries.
+    for each qualification. The box plot also shows outlier salaries.
 """
 
-# plotting a box plot of each qualification
-plt.figure()                             
+# Defining values of data and header to be plotted in box plot function
+
 data = [t_grad_annual_wage['Graduates'], t_grad_annual_wage['Apprenticeship'],
+     t_grad_annual_wage['A level'], t_grad_annual_wage['A* to C grade GCSE']]
+    
+qualifcations = ['Graduate', 'Apprenticeship', 'A level', 'GCSE']
+ 
+
+"""
+Defining the function to create the box plot that takes in a data frame 
+and a list of headers
+"""
+
+def grad_box_plot(dt,header):
+
+    plt.figure()                             
+    data = [t_grad_annual_wage['Graduates'], t_grad_annual_wage['Apprenticeship'],
         t_grad_annual_wage['A level'], t_grad_annual_wage['A* to C grade GCSE']]
        
-qualifcations = ['Graduate', 'Apprenticeship', 'A level', 'GCSE']
-plt.boxplot(data, labels = qualifcations )
-plt.legend() 
-plt.ylim(10000, 40000)
-plt.title(' Box plot of Grad Salaries in UK(2017) per Highest Qualification')
-plt.savefig('Assign1_Boxplot.png')
-plt.show()
+    qualifcations = ['Graduate', 'Apprenticeship', 'A level', 'GCSE']
+    plt.boxplot(data, labels = qualifcations )
+    plt.legend() 
+    plt.ylim(10000, 40000)
+    plt.title(' Box plot of Grad Salaries in UK(2017) per Highest Qualification')
+    plt.savefig('Assign1_Boxplot.png')
+    plt.show()
 
+    return
+
+# Calling the function grad_box_plot with data and qualifications
+
+grad_box_plot(data, qualifcations)
 
 
 
 """
-The third graph shows the highest annual wage for each qualification as a 
-bar graph. The maximum and minimum wages for each qualification is stored in a variable
+The third graph shows the highest annual wage for each qualification as a bar
+ graph.The maximum and minimum wages for each qualification is stored in a variable
 """
 
-graduate_max = t_grad_annual_wage['Graduates'].max()
-apprentice_max = t_grad_annual_wage['Apprenticeship'].max()
-a_level_max = t_grad_annual_wage['A level'].max()
-GCSE_max = t_grad_annual_wage['A* to C grade GCSE'].max()
+def grad_bar_plot(df):
+    graduate_max = t_grad_annual_wage['Graduates'].max()
+    apprentice_max = t_grad_annual_wage['Apprenticeship'].max()
+    a_level_max = t_grad_annual_wage['A level'].max()
+    GCSE_max = t_grad_annual_wage['A* to C grade GCSE'].max()
 
 
-graduate_min = t_grad_annual_wage['Graduates'].min()
-apprentice_min = t_grad_annual_wage['Apprenticeship'].min()
-a_level_min = t_grad_annual_wage['A level'].min()
-GCSE_min = t_grad_annual_wage['A* to C grade GCSE'].min()
+    graduate_min = t_grad_annual_wage['Graduates'].min()
+    apprentice_min = t_grad_annual_wage['Apprenticeship'].min()
+    a_level_min = t_grad_annual_wage['A level'].min()
+    GCSE_min = t_grad_annual_wage['A* to C grade GCSE'].min()
 
 
 # The maximum annual wages are stored in an array as data
-data_max = np.array([graduate_max,apprentice_max, a_level_max, GCSE_max])
-data_min = np.array([graduate_min,apprentice_min, a_level_min, GCSE_min])
+    data_max = np.array([graduate_max,apprentice_max, a_level_max, GCSE_max])
+    data_min = np.array([graduate_min,apprentice_min, a_level_min, GCSE_min])
 
 
 # Plotting the bar graph and adding the title
 
-plt.figure(figsize=(7,5))
+    plt.figure(figsize=(7,5))
 
-plt.ylim(0,40000)
-plt.xticks(rotation=20)
-plt.bar(qualifcations, data_max, label  = 'Highest Salaries', width = 0.5)
-plt.bar(qualifcations, data_min, label = 'Lowest Salaries', color = 'brown', width = 0.5)
-plt.legend()
-plt.title('Highest and Lowest Annual Wages per Qualification')
+    plt.ylim(0,40000)
+    plt.xticks(rotation=20)
+    plt.bar(qualifcations, data_max, label  = 'Highest Salaries', width = 0.5)
+    plt.bar(qualifcations, data_min, label = 'Lowest Salaries', color = 'brown', width = 0.5)
+    plt.legend()
+    plt.title('Highest and Lowest Annual Wages per Qualification')
 
-plt.savefig('Assign1_Bargraph.png')
+    plt.savefig('Assign1_Bargraph.png')
 
-plt.show()
+    plt.show()
+
+    return
 
 
+# Calling the function grad bar plot
 
+grad_bar_plot(t_grad_annual_wage)
 
 
